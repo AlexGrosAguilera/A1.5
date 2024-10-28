a4ca5299e80da2644287ce52d3e925562abf0589
AlexGrosAguilera <a2409829@institutmontilivi.cat>
2024-10-28 11:45:13 +0100

    Primer commit

---
+++ EX1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MARCA_FI = 0;
+            int dada;
+            int nMissatges;
+            nMissatges = 0;
+            dada = Convert.ToInt32(Console.ReadLine());
+            while (dada != MARCA_FI)
+            {
+                nMissatges = nMissatges + 1;
+                Console.WriteLine($"Iteració {nMissatges} --> {dada}");
+                dada = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Total missatges : {nMissatges}");
+        }
+    }
+}
---
+++ Exemple1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exemple1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string MARCA_FI = "adeu";
+            string dada;
+            int nMissatges;
+            nMissatges = 0;
+            dada = Console.ReadLine();
+            while (dada != MARCA_FI) 
+            {
+                nMissatges = nMissatges + 1;
+                Console.WriteLine($"Iteració {nMissatges} --> {dada}");
+                dada = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total missatges : {nMissatges}");
+        }
+    }
+}
